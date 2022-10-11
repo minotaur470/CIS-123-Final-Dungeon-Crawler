66b86f9f195ae2a9a6d974b4e0d75f4ff6fd490c
minotaur470 <minotaur470@users.noreply.github.com>
2022-10-11 17:14:01 -0400

    Initialized main.py
    Added basic information to main.py and included final project specifications within the project file.

---
+++ main.py
@@ -0,0 +1,7 @@
+#CIS123 Final Project - Dominic Quist
+
+#This project is a demonstration of basic principles learned throughout the
+#course. It will be a basic game consisting of basic ASCII UI elements and 
+#a rudimentary storyline. 
+
+#import modules
